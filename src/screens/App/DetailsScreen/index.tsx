6e912652a80776c02506f4846c1f0ccabcf1609c
Thevaraja sarujan <sk_sarujan@Thevarajas-MacBook-Pro.local>
2024-02-25 03:14:07 +0530

    change

--- src/screens/App/DetailsScreen/index.tsx
+++ src/screens/App/DetailsScreen/index.tsx
@@ -1,10 +1,11 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { ScrollView, StatusBar, Text, TouchableWithoutFeedback, View } from 'react-native'
+import { ScrollView, StatusBar, Text, TouchableOpacity, TouchableWithoutFeedback, View } from 'react-native'
 import React, { useState } from 'react'
-import { COLORS } from '@/src/theme/theme'
+import { COLORS, FONTSIZE, SPACING } from '@/src/theme/theme'
 import { styles } from './style'
 import ImageBackgroundInfo from '@/src/component/ImageBackgroundInfo'
 import { useStore } from '@/src/feature/store/store'
+import PaymentFooter from '@/src/component/PaymentFooter'
 
 const DetailsScreen = ({ navigation, route }: any) => {
   console.log('DetailsScreen', route)
@@ -15,6 +16,8 @@ const DetailsScreen = ({ navigation, route }: any) => {
       route.params?.type === 'Coffee' ? state.CoffeeList : state.BeanList,
   )[route.params?.index]
 
+  const [price, setPrice] = useState(ItemOfIndex.prices[0])
+
   const addToFavoriteList = useStore((state: any) => state.addToFavoriteList)
 
   const deleteFromFavoriteList = useStore((state: any) => state.deleteFromFavoriteList)
@@ -22,8 +25,6 @@ const DetailsScreen = ({ navigation, route }: any) => {
   const ToggleFavorite = ( favourite: boolean, type: string, id: string) => {
     favourite ?  deleteFromFavoriteList(type, id) : addToFavoriteList(type, id)
   }
-
-
 
   const BackHandler = () => {
     navigation.pop()
@@ -54,27 +55,73 @@ const DetailsScreen = ({ navigation, route }: any) => {
           ToggleFavorite = {ToggleFavorite}
         />
 
-         <View style = {styles.FooterInfoArea}>
+        <View style = {styles.FooterInfoArea}>
            <Text style = {styles.InfoTitle}> Description </Text>
-           {fullDesc ? 
+            {fullDesc ? 
               (
                 <TouchableWithoutFeedback 
                   onPress = {() => {setFullDesc(prev => !prev)}}
                 >
                     <Text style = {styles.DescriptionText}> {ItemOfIndex?.description} </Text>
                 </TouchableWithoutFeedback>
-              ) : (
-                <TouchableWithoutFeedback  
-                  onPress = {() => {setFullDesc(prev => !prev)}}
+              ) : 
+                (
+                  <TouchableWithoutFeedback  
+                    onPress = {() => {setFullDesc(prev => !prev)}}
+                  >
+                    <Text numberOfLines = {3} style = {styles.DescriptionText}> {ItemOfIndex?.description} </Text>
+                  </TouchableWithoutFeedback>
+                )
+            }
+            <View style = {{paddingTop: SPACING.space_12}}>
+              <Text style = {[styles.InfoTitle, ]}> Size </Text>
+            </View>
+            
+            <View style = {styles.SizeOuterContainer}>
+              {ItemOfIndex.prices?.map((data: any) => (
+                <TouchableOpacity 
+                  key = {data.size}
+                  onPress = {() => {
+                    setPrice(data);
+                  }}
+                  style = {[
+                    styles.SizeBox, 
+                    {
+                      borderBlockColor: 
+                        data.size === price.size 
+                          ? COLORS.primaryOrangeHex
+                          : COLORS.primaryDarkGreyHex
+                    }
+                  ]}
                 >
-                  <Text numberOfLines = {3} style = {styles.DescriptionText}> {ItemOfIndex?.description} </Text>
-                </TouchableWithoutFeedback>
-              )
-           }
-         </View>
+                  <Text 
+                    style = {[
+                      styles.SizeText,
+                      {
+                        fontSize: ItemOfIndex.type === 'bean' 
+                          ? FONTSIZE.size_18
+                          : FONTSIZE.size_24,
+
+                        color: data.size === price.size 
+                          ? COLORS.primaryOrangeHex
+                          : COLORS.primaryRedHex
+                      }
+                    ]}
+                  > 
+                    {data.size} 
+                  </Text>
+                </TouchableOpacity>
+              ))}
+            </View>
+        </View>
+
+        <PaymentFooter
+          price = {price}
+          buttonTitle = 'Add to Cart'
+          buttonPressHandler = {() => {}}
+        />
 
       </ScrollView>
-      <Text>DetailsScreen</Text>
     </View>
   )
 }
--- src/screens/App/DetailsScreen/style.tsx
+++ src/screens/App/DetailsScreen/style.tsx
@@ -1,5 +1,5 @@
 import {StyleSheet} from 'react-native'
-import { COLORS, FONTFAMILY, FONTSIZE, SPACING } from '@/src/theme/theme';
+import { BORDERRADIUS, COLORS, FONTFAMILY, FONTSIZE, SPACING } from '@/src/theme/theme';
 
 export const styles = StyleSheet.create({
     ScreenContainer: {
@@ -15,7 +15,7 @@ export const styles = StyleSheet.create({
     },
     InfoTitle: {
       fontFamily: FONTFAMILY.poppins_semibold,
-      fontSize: FONTSIZE.size_20,
+      fontSize: FONTSIZE.size_18,
       color: COLORS.primaryWhiteHex,
       paddingLeft: SPACING.space_10,
       paddingBottom: SPACING.space_4,
@@ -23,11 +23,29 @@ export const styles = StyleSheet.create({
     DescriptionText: {
       letterSpacing: 0.8,
       fontFamily: FONTFAMILY.poppins_regular,
-      fontSize: FONTSIZE.size_16,
+      fontSize: FONTSIZE.size_14,
       color: COLORS.primaryWhiteHex,
       paddingLeft: SPACING.space_16,
       paddingHorizontal: SPACING.space_12,
       paddingVertical: SPACING.space_12,
       // backgroundColor: COLORS.primaryOrangeHex,
+    },
+    SizeOuterContainer: {
+      flex: 1,
+      flexDirection: 'row',
+      justifyContent:'space-between',
+      gap:SPACING.space_20,
+    },
+    SizeBox: {
+      flex: 1,
+      backgroundColor: COLORS.primaryDarkGreyHex,
+      borderRadius: BORDERRADIUS.radius_15,
+      height: SPACING.space_24 * 2 ,
+      justifyContent:'center',
+      alignItems: 'center',
+      borderWidth: 2,
+    }, 
+    SizeText: {
+      fontFamily: FONTFAMILY.poppins_regular,
     }
 })
