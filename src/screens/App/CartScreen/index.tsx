08971153b98144126344c04ab8fd700660ab092d
@sarujan1 <sarujan@codelantic.com>
2024-02-28 05:47:01 +0530

    change

--- src/screens/App/CartScreen/index.tsx
+++ src/screens/App/CartScreen/index.tsx
@@ -1,18 +1,61 @@
- import { StyleSheet, Text, View } from 'react-native'
+ import { ScrollView, StatusBar, Text, View } from 'react-native'
  import React from 'react'
 import { useStore } from '@/src/feature/store/store'
+import {useBottomTabBarHeight} from '@react-navigation/bottom-tabs';
+import { styles } from './styles';
+import { COLORS } from '@/src/theme/theme';
+import HeaderBar from '@/src/component/HeaderBar';
+import EmptyListAnimation from '@/src/component/EmptyListAnimation';
  
  const CartScreen = () => {
   const CartList = useStore((state: any) => state.CartList) 
+  const CartPrice = useStore((state: any) => state.CartPrice)
+
+  const incrementCartItemQuantity = useStore(
+    (state: any) => state.incrementCartItemQuantity)
+
+  const decrementCartItemQuantity = useStore(
+    (state: any) => state.decrementCartItemQuantity)
+
+  const calculateCartPrice = useStore((state: any) => state.calculateCartPrice)
+
   console.log('CoffeeList********CartList***', CartList.length)
+  console.log('CartPrice********CartPrice***', CartPrice)
+
+  const tabBarHeight = useBottomTabBarHeight();
 
    return (
-     <View>
-       <Text>CartScreen</Text>
+     <View style = {styles.ScreenContainer}>
+        <StatusBar backgroundColor= {COLORS.primaryBlackHex} />
+        <ScrollView
+          showsVerticalScrollIndicator = {false}
+          contentContainerStyle = {styles.ScrollViewFlex}
+        >
+          {/* App Header */}
+          <View 
+            style = { 
+              [
+                styles.ScrollViewInnerView , 
+                {marginBottom: tabBarHeight}
+              ]
+            }
+          >
+            <View style = {styles.ItemContainer}>
+              <HeaderBar title = 'Cart' />
+              {CartList.length === 0 ? 
+                (
+                  <EmptyListAnimation title = {'Cart Is Empty'} />
+                ) : 
+                  (
+                    <></>
+                  )
+              }
+            </View>
+          </View>
+            
+        </ScrollView>
      </View>
    )
  }
  
  export default CartScreen
- 
- const styles = StyleSheet.create({})
---
+++ src/screens/App/CartScreen/styles.tsx
@@ -0,0 +1,22 @@
+import { COLORS } from '@/src/theme/theme'
+import { StyleSheet} from 'react-native'
+
+
+
+export const styles = StyleSheet.create({
+    ScreenContainer: {
+      flex: 1,
+      backgroundColor: COLORS.primaryBlackHex,
+    },
+    ScrollViewFlex: {
+        flexGrow: 1,
+    },
+    ScrollViewInnerView: {
+        flex: 1,
+        justifyContent: 'space-between',
+        // backgroundColor: 'red'
+    },
+    ItemContainer: {
+        flex: 1,
+    }
+})
